afe660a40b44cf53b9cb3bad739acb234553b8eb
Kirill <mudruy.ka@gmail.com>
2023-08-22 14:41:04 +0400

    Сделал дз на 3-тий семинар

---
+++ task19/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да
+*/
+System.Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int start = 0;
+int start1 = 0;
+int end = 0;
+int end1 = 0;
+
+if (number > 9999 && number < 100000)
+{
+    start = number / 10000;
+    start1 = (number / 1000) % 10;
+    end = (number / 10) % 10;
+    end1 = number % 10;
+
+    if ((start == end1 && start1 == end) || (start == end && start1 == end1))
+    {
+        Console.WriteLine("Да");
+    }
+    else
+    {
+        Console.WriteLine("Нет");
+    }
+}
+else
+{
+    Console.WriteLine("Число не пятизначное");
+}
---
+++ task21/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53
+*/
+Console.WriteLine("Введите координаты двух точек: ");
+
+int numberX1 = Convert.ToInt32(Console.ReadLine());
+int numberY1 = Convert.ToInt32(Console.ReadLine());
+int numberZ1 = Convert.ToInt32(Console.ReadLine());
+int numberX2 = Convert.ToInt32(Console.ReadLine());
+int numberY2 = Convert.ToInt32(Console.ReadLine());
+int numberZ2 = Convert.ToInt32(Console.ReadLine());
+
+double numberXY = Math.Sqrt((numberX2 - numberX1)*(numberX2 - numberX1) + (numberY2-numberY1)*(numberY2-numberY1)+ (numberZ2-numberZ1)*(numberZ2-numberZ1));
+double roundedNumber = Math.Round(numberXY, 3); 
+Console.WriteLine(roundedNumber);
