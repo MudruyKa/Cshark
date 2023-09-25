f2c8ee41ae26a0b7a5632ffe1239ebf485645177
Kirill <mudruy.ka@gmail.com>
2023-09-25 22:20:32 +0400

    Домашняя работа по последнему 9-му уроку

---
+++ Homework/9Seminar/task66/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+Console.Clear();
+Console.Write("Введите M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int SumNumbers (int m, int n)
+{
+if(m >= n)
+return n;
+else
+return m+ SumNumbers(m+1,n);
+}
+int result= SumNumbers(m,n);
+Console.WriteLine(result);
---
+++ task60-70/task69/Program.cs
@@ -0,0 +1,11 @@
+﻿/* 69. Напишите программу, которая на вход принимает два числа А и В, и возводит число А в целую степень в с помощью рекурсии.
+*/
+int result = ProdNumber(5,2);
+int ProdNumber (int n, int m)
+{
+if(m==1)
+return n;
+else
+return n*ProdNumber (n,m-1);
+}
+Console.WriteLine(result);
