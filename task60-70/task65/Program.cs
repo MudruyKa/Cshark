52bbc17aebdfa27036b2a6248c95e9dbc243400c
Kirill <mudruy.ka@gmail.com>
2023-09-25 21:36:28 +0400

    Конец 9 урока

---
+++ task60-70/task65/Program.cs
@@ -0,0 +1,12 @@
+﻿
+/* 65. Задайте значения М и N. Напишите программу, которая выведет все натуральные числа в промежутке от М до N.
+*/
+void NuturalNum(int m, int n)
+{
+    if (m < n)
+    {
+        System.Console.write(m + ",");
+        NuturalNum(m + 1, n);
+    }
+}
+NuturalNum(1, 5);
